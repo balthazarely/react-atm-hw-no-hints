7e8ed91a840412cc2c3cfaeeac5c0e2d16ac2d3a
Balthazar Ely <balthazarely@Balthazars-MacBook-Pro.local>
2019-08-07 17:07:18 -0600

    added new hw to this repe

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,17 +2,49 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(){
+    super();
+    this.state = {
+      balance: 0
+    }
+  }
+
+  depositFunction = (e) => {
+    const depositCash = parseInt(this.refs.amount.value);
+
+    this.setState({
+      balance: this.state.balance + depositCash
+    })
+  }
+
+  withdrawFunction = (e) => {
+    const withdrawCash = parseInt(this.refs.amount.value);
+    if(withdrawCash > this.state.balance) {
+      return false
+    }
+    this.setState({
+      balance: this.state.balance - withdrawCash
+    })
+  } 
+
+  
+
   render() {
+    
+    let className = "balance";
+    if(this.state.balance === 0) {
+      className = 'zero';
+    }
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className={className}>${this.state.balance}</div>
+        <input ref="amount" type="number" placeholder="enter an amount" />
+        <button onClick={this.depositFunction}>Deposit</button>
+        <button onClick={this.withdrawFunction}>Withdrawl</button>
       </div>
     )
   }
 }
 
-export default Account;
+export default Account
